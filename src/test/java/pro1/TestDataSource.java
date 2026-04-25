2c37299501d0bafb0c35a36e5bcc424ec8c24bf1
striketr1 <vancura.stanislav@seznam.cz>
2026-04-25 21:30:44 +0200

    změna TestDataSource podle zadání

--- src/test/java/pro1/TestDataSource.java
+++ src/test/java/pro1/TestDataSource.java
@@ -1,30 +1,39 @@
 package pro1;
 
-import java.nio.file.Files;
-
 public class TestDataSource implements DataSource {
+
     @Override
     public String getRozvrhByKatedra(String rok, String katedra) {
         return ResourcesUtils.readResourceFile(
-                String.format("testData/getRozvrhByKatedra_%s_%s.json",rok,katedra));
+                String.format("testData/getRozvrhByKatedra_%s_%s.json", rok, katedra));
 
     }
 
     @Override
     public String getPredmetyByUcitel(String rok, int ucitIdno, String katedra) {
         return ResourcesUtils.readResourceFile(
-                String.format("testData/getPredmetyByUcitel_%s_%s_%s.json",rok,ucitIdno,katedra));
+                String.format("testData/getPredmetyByUcitel_%s_%s_%s.json", rok, ucitIdno, katedra));
 
     }
 
     @Override
     public String getLiteraturaPredmetu(String zkratka, String katedra) {
         return ResourcesUtils.readResourceFile(
-                String.format("testData/getLiteraturaPredmetu_%s_%s.json",zkratka,katedra));
+                String.format("testData/getLiteraturaPredmetu_%s_%s.json", zkratka, katedra));
     }
 
     @Override
     public String getTerminyZkousek(String semestr, String zkratka, String katedra) {
         return "";
+    }
+
+    public String getTerminyZkousek2(String katedra) {
+        return ResourcesUtils.readResourceFile(
+                String.format("testData/getTerminyZkousek_%s.json", katedra));
+    }
+
+    public String getKvalifikacniPrace(String rokObhajoby, String katedra) {
+        return ResourcesUtils.readResourceFile(
+                String.format("testData/getKvalifikacniPrace_%s_%s.json", rokObhajoby, katedra));
     }
 }
